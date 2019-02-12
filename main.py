52bb541f355d594478fae0fb5bd1a85e9ffeb1fa
Cleidimar Santos <cleidimar.santos@dekra.com>
2019-02-12 19:26:08 -0200

    change

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 import os
 import datetime
-import webbrowser
-
-
-print("\n ----------------------\n| Start monitor PyEyes |\n ----------------------") 
 
 class Watcher:
 
@@ -19,13 +15,13 @@ class Watcher:
         logging.basicConfig(level=logging.INFO,
                             format='%(asctime)s - %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
+
         path = sys.argv[1] if len(sys.argv) > 1 else '.'
 
         if os.path.isdir(path):
             event_handler = LoggingEventHandler()
             self.observer.schedule(event_handler, path, recursive=True)
             self.observer.start()
-            self.renewPage(logging)
             
             try:
                 while True:
@@ -35,21 +31,11 @@ class Watcher:
                 self.observer.join()
         else:
             print("\nIncorrect directory or path. Note that it has been entered correctly! :-(")
-        
 
-    def renewPage(self, text):
-        f = open('helloworld.html','w')
+#print("\n ----------------------\n| Start monitor PyEyes |\n ----------------------") 
 
-        message = """<html>
-        <head></head>
-        <body><p>{text}</p></body>
-        </html>"""
 
-        f.write(message)
-        f.close()
-        return webbrowser.open_new_tab('helloworld.html')
-
-if __name__ == "__main__":
+if __name__ == "__main__":        
     w = Watcher()
     w.run()
 
